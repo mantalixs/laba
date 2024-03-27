93497d3b64b65c7e545eb8b9e5426a868096af0e
Maxim Navrotskiy <navrotskyi.maksym@lll.kpi.ua>
2024-03-27 13:19:40 +0200

    Add files via upload

---
+++ laba/proga 1/proga 1/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+public class Day
+{
+    public int DayOfMonth { get; set; }
+
+    public Day(int dayOfMonth)
+    {
+        DayOfMonth = dayOfMonth;
+    }
+
+    public override string ToString()
+    {
+        return $"{DayOfMonth}";
+    }
+}
+
+public class Month
+{
+    public int MonthNumber { get; set; }
+
+    public Month(int monthNumber)
+    {
+        MonthNumber = monthNumber;
+    }
+
+    public override string ToString()
+    {
+        return $"{MonthNumber}";
+    }
+}
+
+public class Year
+{
+    public int YearNumber { get; set; }
+
+    public Year(int yearNumber)
+    {
+        YearNumber = yearNumber;
+    }
+
+    public override string ToString()
+    {
+        return $"{YearNumber}";
+    }
+}
+
+public class Date : Year
+{
+    public Month Month { get; set; }
+    public Day Day { get; set; }
+
+    public Date(int year, int month, int day) : base(year)
+    {
+        Month = new Month(month);
+        Day = new Day(day);
+    }
+
+    public string GetDayOfWeek()
+    {
+        return "Day of the week"; 
+    }
+
+    public int GetDaysDifference(Date otherDate)
+    {
+        int daysInMonth = 30; 
+        int thisDays = this.Day.DayOfMonth + this.Month.MonthNumber * daysInMonth + this.YearNumber * 365;
+        int otherDays = otherDate.Day.DayOfMonth + otherDate.Month.MonthNumber * daysInMonth + otherDate.YearNumber * 365;
+        return Math.Abs(otherDays - thisDays);
+    }
+
+    public int GetMonthsDifference(Date otherDate)
+    {
+        int monthsInYear = 12;
+        int thisMonths = this.Month.MonthNumber + this.YearNumber * monthsInYear;
+        int otherMonths = otherDate.Month.MonthNumber + otherDate.YearNumber * monthsInYear;
+        return Math.Abs(otherMonths - thisMonths);
+    }
+
+    public int GetYearsDifference(Date otherDate)
+    {
+        return Math.Abs(otherDate.YearNumber - this.YearNumber);
+    }
+
+    public override string ToString()
+    {
+        return $"{Day}.{Month}.{YearNumber}";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the first date (year, month, day):");
+        string[] input1 = Console.ReadLine().Split();
+        int year1 = int.Parse(input1[0]);
+        int month1 = int.Parse(input1[1]);
+        int day1 = int.Parse(input1[2]);
+        Date date1 = new Date(year1, month1, day1);
+
+        Console.WriteLine("Enter the second date (year, month, day):");
+        string[] input2 = Console.ReadLine().Split();
+        int year2 = int.Parse(input2[0]);
+        int month2 = int.Parse(input2[1]);
+        int day2 = int.Parse(input2[2]);
+        Date date2 = new Date(year2, month2, day2);
+
+        Console.WriteLine($"Difference in years: {date1.GetYearsDifference(date2)}");
+        Console.WriteLine($"Difference in months: {date1.GetMonthsDifference(date2)}");
+        Console.WriteLine($"Difference in days: {date1.GetDaysDifference(date2)}");
+    }
+}
---
+++ laba/proga 2/proga 2/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Vessel
+{
+    public string Name { get; set; }
+    public int Year { get; set; }
+    public double FuelConsumption { get; set; }
+
+    public Vessel(string name, int year, double fuelConsumption)
+    {
+        Name = name;
+        Year = year;
+        FuelConsumption = fuelConsumption;
+    }
+}
+
+public class TankerShip : Vessel
+{
+    public double CargoCapacity { get; set; }
+
+    public TankerShip(string name, int year, double fuelConsumption, double cargoCapacity)
+        : base(name, year, fuelConsumption)
+    {
+        CargoCapacity = cargoCapacity;
+    }
+}
+
+public class CargoShip : Vessel
+{
+    public double CargoCapacity { get; set; }
+
+    public CargoShip(string name, int year, double fuelConsumption, double cargoCapacity)
+        : base(name, year, fuelConsumption)
+    {
+        CargoCapacity = cargoCapacity;
+    }
+}
+
+public class Carrier
+{
+    public List<Vessel> Vessels { get; set; }
+
+    public Carrier()
+    {
+        Vessels = new List<Vessel>();
+    }
+
+    public void AddVessel(Vessel vessel)
+    {
+        Vessels.Add(vessel);
+    }
+
+    public double CalculateTotalCargoCapacity()
+    {
+        return Vessels.Sum(vessel => (vessel as TankerShip)?.CargoCapacity ?? 0 + (vessel as CargoShip)?.CargoCapacity ?? 0);
+    }
+
+    public double CalculateTotalFuelConsumption()
+    {
+        return Vessels.Sum(vessel => vessel.FuelConsumption);
+    }
+
+    public void SortVesselsByFuelConsumption()
+    {
+        Vessels.Sort((x, y) => x.FuelConsumption.CompareTo(y.FuelConsumption));
+    }
+
+    public List<Vessel> FindVesselsByYearRange(int startYear, int endYear)
+    {
+        return Vessels.Where(vessel => vessel.Year >= startYear && vessel.Year <= endYear).ToList();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Carrier carrier = new Carrier();
+
+        carrier.AddVessel(new TankerShip("Tanker 1", 2000, 100, 5000));
+        carrier.AddVessel(new TankerShip("Tanker 2", 1995, 150, 7000));
+        carrier.AddVessel(new CargoShip("Cargo Ship 1", 2010, 80, 10000));
+        carrier.AddVessel(new CargoShip("Cargo Ship 2", 2005, 90, 12000));
+
+        Console.WriteLine($"Total cargo capacity of the carrier: {carrier.CalculateTotalCargoCapacity()} tons");
+        Console.WriteLine($"Total fuel consumption of the carrier: {carrier.CalculateTotalFuelConsumption()} liters per hour");
+
+        carrier.SortVesselsByFuelConsumption();
+
+        List<Vessel> vesselsInRange = carrier.FindVesselsByYearRange(2000, 2010);
+        Console.WriteLine("Vessels within the specified year range:");
+        foreach (var vessel in vesselsInRange)
+        {
+            Console.WriteLine($"Name: {vessel.Name}, Year: {vessel.Year}");
+        }
+    }
+}
